33a5b3fc3d69ae0a04a2349700912c09206665b2
jerome <jerome.macias@gmail.com>
2012-01-17 22:35:09 +0100

    Added validates_presence_of to model for ProjectsUsers and StagesUser

--- app/models/projects_users.rb
+++ app/models/projects_users.rb
@@ -1,2 +1,3 @@
 class ProjectsUsers < ActiveRecord::Base
+  validates_presence_of :project_id, :user_id
 end
--- app/models/stages_user.rb
+++ app/models/stages_user.rb
@@ -1,4 +1,6 @@
 class StagesUser < ActiveRecord::Base
   belongs_to :stage
   belongs_to :user
+
+  validates_presence_of :stage_id, :user_id
 end
