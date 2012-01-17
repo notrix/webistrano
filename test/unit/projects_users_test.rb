91fb238ccbef5a79257838602f6763064769ae96
jerome <jerome.macias@gmail.com>
2012-01-17 22:33:48 +0100

    Added utnit tests for ProjectsUsers

--- test/unit/projects_users_test.rb
+++ test/unit/projects_users_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class ProjectsUsersTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  fixtures :projects
+  fixtures :users
+
+  def setup
+    @project = projects(:project_1)
+    @user = users(:quentin)
+  end
+
+  def test_creation
+    assert_equal 0, ProjectsUsers.count
+
+    assert_nothing_raised{
+      pu = ProjectsUsers.create!(:project_id => @project.id, :user_id => @user.id)
+    }
+
+    assert_equal 1, ProjectsUsers.count
+  end
+
+  def test_validation
+    # try to create project user without project id
+    pu = ProjectsUsers.new(:user_id => @user.id)
+    assert !pu.valid?
+    assert_not_nil pu.errors.on("project_id")
+
+    # try to create two entries with same project_id and user_id
+    pu = ProjectsUsers.create!(:project_id => @project.id, :user_id => @user.id)
+
+    assert_raise(ActiveRecord::StatementInvalid) {
+      pu = ProjectsUsers.create!(:project_id => @project.id, :user_id => @user.id)
+    }
   end
 end
