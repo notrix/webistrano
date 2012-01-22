7e1b9bb2b90684c92ce79307279964382dc2d1ea
jerome <jerome.macias@gmail.com>
2012-01-22 01:08:53 +0100

    Restore loadWindow for capfile export link [ci skip]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -91,6 +91,12 @@ $(function(){
 
 });
 
+// open a new window
+function loadWindow(url) {
+  popupWin = window.open(url, 'new_window', 'scrollbars=no,status=no,toolbar=no,location=no,directories=no,menubar=no,width=960, height=700, top=50, left=50, resizable=1, scrollbars=yes');
+  popupWin.focus();
+}
+
 /* Create Menu Links */
 
 function open_menu(dom_id){
